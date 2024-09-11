6cd40b2bed720b883a6b3c701385e6c2e2a8633d
Tal Shmain <thbuhtav1122@gmail.com>
2024-09-11 11:48:12 +0300

    implement the complete and exit buttons

--- file_compressor.py
+++ file_compressor.py
@@ -2,18 +2,25 @@ import FreeSimpleGUI as sg
 
 label1 = sg.Text("Select files to compress: ")
 input1 = sg.Input()
-choose_button1 = sg.FilesBrowse("Choose")
+choose_button1 = sg.FilesBrowse("Choose", key="files")
 
-label2 = sg.Text("Selecet destination folder: ")
-inout2 = sg.Input()
-choose_button2 = sg.FolderBrowse("Choose")
+label2 = sg.Text("Select destination folder: ")
+input2 = sg.Input()
+choose_button2 = sg.FolderBrowse("Choose", key="folder")
 compress_button = sg.Button("Compress")
 
 window = sg.Window("File Compressor",
-                    layout=[[label1, input1, choose_button1],
-                            [label2, input2, choose_button2]
-                            [compress_button2]])
-window.read()
+                   layout=[[label1, input1, choose_button1],
+                           [label2, input2, choose_button2],
+                           [compress_button]])
+
+while True:
+    event, values = window.read()
+    print(event, values)
+    filepaths = values["files"].split(";")
+    folder = values["folder"]
+
 window.close()
 
-# sell
+
+
